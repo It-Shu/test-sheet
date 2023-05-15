ecef710651e92c3cb1db7658afdd158d63070ef0
It-Shu <it.shustov@gmail.com>
2023-05-15 17:37:45 +0300

    getdata from docs and convert to array

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {SheetApi} from "./api/sheet-api";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    const [docsText, setDocsText] = useState('')
+    const [objects, setObjects] = useState<string[]>([])
+    const [isActive, setIsActive] = useState(false)
+
+    const columnMap: { [key: string]: string } = {
+        "1": "A",
+        "2": "B",
+        "3": "C",
+        "4": "D",
+        "5": "E",
+        "6": "F",
+        "7": "G",
+        "8": "H",
+        "9": "I",
+        "10": "J",
+    };
+
+    useEffect(() => {
+        SheetApi.getDocsData().then(res => setDocsText(res.data))
+        const regex = /{([^{}]+)}/g;
+        const matches = docsText.match(regex);
+
+        if (matches !== null) {
+            setObjects(matches.map((match) => {
+                return match.substring(1, match.length - 1);
+            }));
+        }
+        console.log(objects)
+
+        // for (const result of objects) {
+        //     const [list, column, line, endColumn, endLine] = result.split("-");
+        //
+        // }
+        const resultArrays = objects.map(result => result.split("-"));
+
+        const results = resultArrays.map(([list, column, line, endColumn, endLine]) => ({
+            list,
+            column: columnMap[column] || column,
+            line,
+            endColumn: columnMap[endColumn] || endColumn,
+            endLine,
+        }));
+        console.log(results);
+    }, [isActive])
+
+
+
+        return (
+            <div className="App">
+                <button onClick={() => setIsActive(!isActive)}>Get</button>
+                <pre>{objects}</pre>
+            </div>
+        );
+    }
+
 
 export default App;
